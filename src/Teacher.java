5b324f0bc0351fc22103cbc0e4094e9e1ac7b3cd
yacine2410 <yacin.montacer@gmail.com>
2023-01-23 18:48:36 +0100

    Pre-including grades

--- src/Teacher.java
+++ src/Teacher.java
@@ -2,11 +2,13 @@
 public class Teacher extends Person {
     private String department;
     private int yearsExperience;
+    private String[] course;
 
-    public Teacher(String email, int phoneNumber, String firstName, String lastName, String password, int ID, String department, int yearsExperience) {
+    public Teacher(String email, int phoneNumber, String firstName, String lastName, String password, int ID, String department, int yearsExperience, String[] course) {
         super(email, phoneNumber, firstName, lastName, password, ID);
     this.department = department;
     this.yearsExperience = yearsExperience;
+    this.course = course;
     }
 
     public void setDepartment(String department) {
@@ -23,5 +25,13 @@ public class Teacher extends Person {
 
     public int getYearsExperience() {
         return yearsExperience;
+    }
+
+    public String[] getCourse() {
+        return course;
+    }
+
+    public void setCourse(String[] course) {
+        this.course = course;
     }
 }
